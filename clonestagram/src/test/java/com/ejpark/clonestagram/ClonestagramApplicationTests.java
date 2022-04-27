73a2d1a09f18d4f5ccb5ef4fe1dc556f1e8de5cb
4dv3ntur3 <allyep13.07@gmail.com>
2022-04-27 23:20:00 +0900

    2022-04-27 initial setting for project

---
+++ clonestagram/src/test/java/com/ejpark/clonestagram/ClonestagramApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ejpark.clonestagram;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClonestagramApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
